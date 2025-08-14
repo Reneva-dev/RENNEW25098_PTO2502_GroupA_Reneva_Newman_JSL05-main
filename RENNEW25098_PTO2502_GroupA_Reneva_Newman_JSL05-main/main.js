c5913d9aec641ee68c609e77b475e8425c42495f
Reneva Newman <renevanewman7@gmail.com>
2025-08-14 10:33:38 +0300

    adjusted dark mode code in main.js

--- RENNEW25098_PTO2502_GroupA_Reneva_Newman_JSL05-main/main.js
+++ RENNEW25098_PTO2502_GroupA_Reneva_Newman_JSL05-main/main.js
@@ -79,9 +79,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (theme === "dark") {
       document.body.classList.add("dark-theme");
       themeToggleCheckbox.checked = true;
+      if (logo) logo.src = './assets/logo-dark.svg';
     } else {
       document.body.classList.remove("dark-theme");
       themeToggleCheckbox.checked = false;
+      if (logo) logo.src = './assets/logo-light.svg';
     }
     localStorage.setItem("theme", theme);
   }
